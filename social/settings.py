9754784f24af58dc2dd836d53a90d4eca43099f8
Anshul Joshi <63587194+joshianshul2@users.noreply.github.com>
2021-03-18 15:03:56 +0530

    Update settings.py

--- social/settings.py
+++ social/settings.py
@@ -137,11 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
+    os.path.join(BASE_DIR, "static"),
 
+]
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
