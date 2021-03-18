39b413928e8184c465988361840088b5896917f3
Anshul Joshi <63587194+joshianshul2@users.noreply.github.com>
2021-03-18 15:25:00 +0530

    Update urls.py

--- social/urls.py
+++ social/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from socialcustom import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,4 @@ urlpatterns = [
 
  ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+urlpatterns += staticfiles_urlpatterns()
