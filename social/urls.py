1475e903644c6c425ecd0fb359143e583c9dda55
Anshul Joshi <63587194+joshianshul2@users.noreply.github.com>
2021-03-18 14:49:15 +0530

    Update urls.py

--- social/urls.py
+++ social/urls.py
@@ -21,8 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', views.signup,name='signup.html')
     path("", include('socialcustom.urls')),
-    
- if settings.DEBUG == True:
-   urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-]
+
+ ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
